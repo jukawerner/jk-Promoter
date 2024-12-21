32f3932926d60114a84cd4b9b79d366708f91bd0
jukawashimo <jukawerner@msn.com>
2024-12-21 11:59:27 -0300

    v6.9

--- app/admin/cadastro-roteiro/page.tsx
+++ app/admin/cadastro-roteiro/page.tsx
@@ -4,7 +4,6 @@ import { useState, useRef, useEffect } from "react";
 import { GoogleMap, LoadScript, Marker, DirectionsRenderer } from "@react-google-maps/api";
 import { motion } from "framer-motion";
 import { Button } from "@/components/ui/button";
-
 import { Card } from "@/components/ui/card";
 import { createClient } from "@/lib/supabase/client";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
